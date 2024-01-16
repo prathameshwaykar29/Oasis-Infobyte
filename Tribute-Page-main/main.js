713784ceca85ac0ae0b4c3f0bafa547abe08f257
prathameshwaykar29 <141624016+prathameshwaykar29@users.noreply.github.com>
2024-01-16 12:37:26 +0530

    Add files via upload

---
+++ Tribute-Page-main/main.js
@@ -0,0 +1,38 @@
+document.getElementById("title-2").addEventListener("click", function () {
+    let divToBeTargetted = document.getElementById("tribute-info")
+    if (divToBeTargetted.style.display === "none") {
+        divToBeTargetted.style.display = "block";
+    } else {
+        divToBeTargetted.style.display = "none";
+    }
+});
+
+
+document.getElementById("title-3").addEventListener("click", function () {
+    let divToBeTargetted = document.getElementById("timeline")
+    if (divToBeTargetted.style.display === "none") {
+        divToBeTargetted.style.display = "block";
+    } else {
+        divToBeTargetted.style.display = "none";
+    }
+});
+
+
+document.getElementById("title-4").addEventListener("click", function () {
+    let divToBeTargetted = document.getElementById("gallery")
+    if (divToBeTargetted.style.display === "none") {
+        divToBeTargetted.style.display = "block";
+    } else {
+        divToBeTargetted.style.display = "none";
+    }
+});
+
+
+document.getElementById("title-5").addEventListener("click", function () {
+    let divToBeTargetted = document.getElementById("connect")
+    if (divToBeTargetted.style.display === "none") {
+        divToBeTargetted.style.display = "block";
+    } else {
+        divToBeTargetted.style.display = "none";
+    }
+});
